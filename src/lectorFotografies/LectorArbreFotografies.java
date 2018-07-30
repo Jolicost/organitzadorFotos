11b50598ed73404966f078dc896109605078574b
Jolicost <costasca6@gmail.com>
2018-07-30 12:52:15 +0200

    arbre carpetes

--- src/lectorFotografies/LectorArbreFotografies.java
+++ src/lectorFotografies/LectorArbreFotografies.java
@@ -1,5 +1,16 @@
 package lectorFotografies;
 
+import java.io.File;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+import org.apache.commons.io.filefilter.NotFileFilter;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+
+import model.Fotografia;
+
 public class LectorArbreFotografies extends LectorCarpetaFotografies{
 
 	public LectorArbreFotografies(String folder) {
@@ -9,5 +20,13 @@ public class LectorArbreFotografies extends LectorCarpetaFotografies{
 
 	public void llegir(){
 		super.llegir();
+		for (File f:
+			(List<File>) FileUtils.listFiles(new File(super.folder), new NotFileFilter(TrueFileFilter.INSTANCE), DirectoryFileFilter.DIRECTORY)
+		) {
+			LectorArbreFotografies l = new LectorArbreFotografies(f.getAbsolutePath());
+			l.llegir();
+			super.resultat.addAll(l.getResultat());
+		}
+	
 	}
 }
